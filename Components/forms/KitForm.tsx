da94c12302160f3740a7cab07985939347188d14
Wilson Fdez <wgfs40@gmail.com>
2025-07-25 18:14:21 -0400

    Implement code changes to enhance functionality and improve performance

---
+++ Components/forms/KitForm.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import Image from "next/image";
+import CustomInput from "../customs/CustomInput";
+import { useState, useEffect } from "react";
+import { SaveCustomerInfo } from "@/actions/customer_info";
+
+const KitForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+    setMessage("");
+
+    try {
+      const formData = new FormData(event.currentTarget);
+      const customerData = {
+        name: formData.get("name") as string,
+        email: formData.get("email") as string,
+      };
+
+      await SaveCustomerInfo(customerData);
+      setMessage("Información del cliente guardada con éxito!");
+      // Reset form
+      //event.currentTarget.reset();
+    } catch (error) {
+      console.error("Error saving customer information:", error);
+      setMessage(
+        "Error al guardar la información del cliente. Por favor, inténtalo de nuevo."
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    if (message) {
+      const timer = setTimeout(() => setMessage(""), 10000);
+      return () => clearTimeout(timer);
+    }
+  }, [message]);
+
+  return (
+    <div className="bg-orange-400 p-6 rounded-lg max-w-md mx-auto">
+      <h1 className="text-4xl font-bold p-5 text-center text-white mb-4">
+        Bienvenido a dosis de marketing
+      </h1>
+      <div className="mb-4 text-center">
+        <Image
+          src="/images/kit-illustration.png"
+          alt="Ilustración de kit"
+          width={128}
+          height={128}
+          className="mx-auto mb-4 w-32 h-auto"
+        />
+      </div>
+      <form className="space-y-4" onSubmit={handleSubmit}>
+        <CustomInput
+          name="name"
+          type="text"
+          placeholder="Introduce tu nombre completo"
+          className="w-full px-4 py-2 border bg-white border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black"
+        />
+        <CustomInput
+          name="email"
+          type="email"
+          placeholder="Introduce tu correo electrónico"
+          className="w-full px-4 py-2 border bg-white border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black"
+        />
+
+        <button
+          disabled={isLoading}
+          type="submit"
+          className="w-full bg-black text-white py-2 rounded hover:bg-gray-800 transition"
+        >
+          {isLoading ? "Guardando..." : " Quiero el Kit"}
+        </button>
+        {message && (
+          <p
+            className={`text-center ${
+              message.includes("Error") ? "text-red-800" : "text-white"
+            }`}
+          >
+            {message}
+          </p>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default KitForm;
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
-import CustomerInfoForm from "@/Components/forms/CustomerInfoForm";
+import KitForm from "@/Components/forms/KitForm";
 
 export default function Home() {
   return (
     <div>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold">Bienbenido a dosis de marketing</h1>
-        <p className="text-lg text-gray-600">
-          Aquí puedes encontrar información sobre nuestros servicios y cómo
-          contactarnos.
-        </p>
-        <CustomerInfoForm />
+      <main className="flex flex-col gap-[32px] items-center justify-center min-h-screen bg-orange-400">
+        <KitForm />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>

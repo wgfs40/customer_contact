4b3a4a67df9df98f58b09d061f8faecd975962d5
Wilson Fdez <wgfs40@gmail.com>
2025-07-29 16:06:07 -0400

    Remove unused Hero component import from Layout to streamline code

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,14 +1,12 @@
 "use client";
 import { useState } from "react";
 import Header from "@/components/customs/Header";
-import Hero from "@/components/customs/Hero";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div>
       <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-     
       {/* Main content area */}
       <div className="container mx-auto px-4">
         {/* Main content */}

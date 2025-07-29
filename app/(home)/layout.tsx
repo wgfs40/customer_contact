908049d989a9d3d08075326f19cff0fce727d68e
Wilson Fdez <wgfs40@gmail.com>
2025-07-29 16:03:38 -0400

    Refactor Layout and Page components to improve structure and maintainability by moving the Hero component to the Page and enhancing the Layout's footer section.

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -8,13 +8,20 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      {/* Hero section */}
-      <Hero />
+     
       {/* Main content area */}
       <div className="container mx-auto px-4">
         {/* Main content */}
         {children}
       </div>
+      {/* Footer section */}
+      <footer className="bg-gray-800 text-white py-4">
+        <div className="container mx-auto px-4">
+          <p className="text-center">
+            © 2023 Dosis de Marketing. Todos los derechos reservados.
+          </p>
+        </div>
+      </footer>
     </div>
   );
 };
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,15 @@
 "use client";
+
+import Hero from "@/components/customs/Hero";
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      {" "}
+      {/* Hero section */}
+      <Hero />
+    </div>
+  );
 };
 
 export default page;

7ee5612a72bef012bf7400953e1dda9ea88f69e4
Wilson Fdez <wgfs40@gmail.com>
2025-07-31 19:53:52 -0400

    feat(sitemap): add next-sitemap configuration and update package dependencies

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://dosisdemarketing.com/",
+  generateRobotsTxt: true,
+  exclude: ["/admin", "/privado"],
+  sitemapSize: 5000,
+};

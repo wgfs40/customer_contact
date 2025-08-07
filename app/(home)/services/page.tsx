956799ea6d529b761b2fee8a6cc1ce66c040921c
Wilson Fdez <wgfs40@gmail.com>
2025-08-07 10:45:22 -0400

    refactor(blog): replace Head component with Metadata export for improved SEO management

--- app/(home)/services/page.tsx
+++ app/(home)/services/page.tsx
@@ -1,22 +1,20 @@
-"use client";
 import Services from "@/components/services/Services";
-import Head from "next/head";
-<Head>
-  <title>Servicios | Dosis de Marketing</title>
-  <meta
-    name="description"
-    content="Descubre nuestros servicios de marketing digital, branding y desarrollo web."
-  />
-  <meta name="robots" content="index, follow" />
-  <meta property="og:title" content="Servicios | Dosis de Marketing" />
-  <meta
-    property="og:description"
-    content="Descubre nuestros servicios de marketing digital, branding y desarrollo web."
-  />
-  <meta property="og:type" content="website" />
-  <meta property="og:image" content="/images/og-image.jpg" />
-  <meta property="og:url" content="https://dosisdemarketing.com/services" />
-</Head>;
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Servicios | Dosis de Marketing",
+  description:
+    "Descubre nuestros servicios de marketing digital, branding y desarrollo web.",
+  robots: "index, follow",
+  openGraph: {
+    title: "Servicios | Dosis de Marketing",
+    description:
+      "Descubre nuestros servicios de marketing digital, branding y desarrollo web.",
+    images: ["/images/logo.jpg"],
+    url: "https://dosisdemarketing.com/services",
+  },
+};
+
 const ServicePage = () => {
   return (
     <div className="min-h-screenflex items-center justify-center px-4">
